2950c051f000360248172bc62fd4d7e220907d57
SHUBHAM LATHIYA <shubhamlathiya2004@gmail.com>
2025-01-31 21:06:07 +0530

    1 Commit message

---
+++ src/main/java/com/codershubham/cms/cms/constant/PathConstant.java
@@ -0,0 +1,16 @@
+package com.codershubham.cms.cms.constant;
+
+public class PathConstant {
+
+
+    public static final String USERS_PATH  = "users";
+
+    public static final String DEPARTMENTS_PATH  = "departments";
+    public static final String COURSES_PATH  = "courses";
+    public static final String SUBJECTS_PATH  = "subjects";
+    public static final String ROLES_PATH  = "roles";
+
+
+
+
+}
