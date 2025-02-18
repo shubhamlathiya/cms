39c51c3e3d24344d215b2b29c84f531ccbe67489
SHUBHAM LATHIYA <shubhamlathiya2004@gmail.com>
2025-02-18 21:55:19 +0530

    29 commit

--- src/main/java/com/codershubham/cms/cms/constant/PathConstant.java
+++ src/main/java/com/codershubham/cms/cms/constant/PathConstant.java
@@ -2,21 +2,23 @@ package com.codershubham.cms.cms.constant;
 
 public class PathConstant {
 
-
+//    user path
     public static final String USERS_PATH = "users";
     public static final String ADMIN_PATH = "admin";
+    public static final String FACULTY_PATH = "faculty";
+    public static final String STUDENTS_PATH = "students";
+
 
     public static final String DEPARTMENTS_PATH =  ADMIN_PATH + "/departments";
-
     public static final String COURSES_PATH = ADMIN_PATH + "/courses";
     public static final String SUBJECTS_PATH = ADMIN_PATH + "/subjects";
     public static final String SYLLABUS_PATH =ADMIN_PATH + "/syllabus";
     public static final String ROLES_PATH =  ADMIN_PATH +  "/roles";
 
-    public static final String FACULTY_PATH = "faculty";
+
     public static final String SEMESTER_PATH = "semesters";
     public static final String ASSIGNMENTS_PATH = "assignments";
-    public static final String STUDENTS_PATH = "students";
+
     public static final String ENROLLMENTS_PATH = "enrollments";
     public static final String SUBJECT_ENROLLMENTS_PATH = "subject-enrollment";
     public static final String FACULTY_SUBJECT_PATH = "faculty-subject";
